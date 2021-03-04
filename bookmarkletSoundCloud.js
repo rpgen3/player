66214006e1a2ad1a074b6df052c96a374f4d426c
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-03-05 08:03:42 +0900

    Update bookmarkletSoundCloud.js

--- bookmarkletSoundCloud.js
+++ bookmarkletSoundCloud.js
@@ -1,1 +1,1 @@
-javascript:["tracks","playlists"].map(v=>document.body.innerHTML.match(new RegExp(v+":([0-9]+)"))).filter(v=>v).forEach((v,i,a)=>prompt(`URL(${i+1}/${a.length})`,`https://api.soundcloud.com/${v[0].replace(/:/,'\/')}`))
+javascript:["tracks","playlists"].map(v=>new RegExp(v+":([0-9]+)")).map(v=>["head","body"].map(e=>document[e].innerHTML.match(v)).filter(v=>v)[0]).filter(v=>v).forEach((v,i,a)=>prompt(`URL(${i+1}/${a.length})`,`https://api.soundcloud.com/${v[0].replace(/:/,'\/')}`))
