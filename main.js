4f7688bf50878f9af95a075b76e3897a69ded456
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-03-05 13:57:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ function loadList(){
                             }).add($("<div>").prependTo(h).text(r.user.username).css({
                                 top: 5,
                                 fontSize: 10,
-                                color: "#999999",
+                                color: "#dddddd",
                                 "text-decoration": "underline"
                             })).css({
                                 padding: 5,
