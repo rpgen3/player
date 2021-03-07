2e4e3162c2aed381fa105e5ed409e32693e3581e
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-03-07 17:04:12 +0900

    Update user.js

--- user.js
+++ user.js
@@ -9,7 +9,7 @@ setTimeout(()=>{
             src: `https://i.imgur.com/${p.imgur}.png`
         });
     }
-},1000);
+},500);
 function makeNewInputURL(value){
     window.inputURL = rpgen3.addInputText($("#hideArea").empty(),Object.assign({
         value: value
